542ff201eca1b916f65f96b9163f94b1f07d47a5
gal2004 <galpalas265@gmail.com>
2019-09-24 11:38:22 +0300

    update project

---
+++ BubbleTravel/DateRangePicker.js
@@ -0,0 +1,19 @@
+/*
+function getDate(){
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = mm + '/' + dd + '/' + yyyy;
+    return today;
+}
+*/
+
+$(function() {
+  $('input[name="daterange"]').daterangepicker({
+    opens: 'left'
+  }, function(start, end, label) {
+    console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+  });
+});
