e0f77b1ee20a3fd86bbaab7fa13f61ded0d0b584
PavelShe19 <48412351+PavelShe19@users.noreply.github.com>
2019-09-24 12:34:47 +0300

    removed hard-coded links and typos

--- BubbleTravel/javascript/Login_and_Registration.js
+++ BubbleTravel/javascript/Login_and_Registration.js
@@ -58,6 +58,6 @@ function logout(){
 }
 
 function backToMainPage(){
-  window.location.href = "file:///C:/Users/galpa/Desktop/%D7%9E%D7%93%D7%A2%D7%99%20%D7%94%D7%9E%D7%97%D7%A9%D7%91/%D7%AA%D7%A7%D7%99%D7%95%D7%AA%20%D7%9C%D7%99%D7%9E%D7%95%D7%93%D7%99%D7%9D/%D7%A9%D7%A0%D7%94%20%D7%A8%D7%91%D7%99%D7%A2%D7%99%D7%AA%20%D7%A1%D7%99%D7%9E%D7%A1%D7%98%D7%A8%20%D7%91/%D7%AA%D7%9B%D7%A0%D7%95%D7%AA%20web/BubbleTravel/MainPage.html";
+  window.location.href = "../html/MainPage.html";
 }
 
